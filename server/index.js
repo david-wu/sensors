3d4fec851bbb844a13caa310c3aa61d9dd85f471
David Wu <david.wuu@gmail.com>
2015-10-08 23:35:42 -0700

    init

--- server/index.js
+++ server/index.js
@@ -2,30 +2,28 @@
 _ = require('lodash');
 Q = require('q');
 
-var QHttp = require('q-io/http');
-var IO = require('socket.io');
-var Client = require('./models/client.js');
 
 var app = App();
-// var server = QHttp.Server(app);
 var server = require('http').createServer(app)
-var io = IO(server);
+
+var io = require('socket.io')(server);
+var Client = require('./models/client.js');
 
 // Creates new Clients when a socket connection is made
 io.on('connection', function(socket){
     var client = new Client(socket);
-})
+});
 
 var port = 5000;
 server.listen(port, function(){
     console.log('Server started port: ' + port);
 });
 
+// Move elsewhere
 function App(){
     var app = require('express')();
     var bodyParser = require('body-parser');
     var apiRouter = require('./api');
-
     app.use(bodyParser.urlencoded({extended:true}));
     app.use(bodyParser.json());
     app.use('/api', apiRouter);
